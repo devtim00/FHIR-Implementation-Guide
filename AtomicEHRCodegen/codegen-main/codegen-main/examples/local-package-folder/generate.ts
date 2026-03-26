2d29e7ce7288a05056d173b4283c77eaa1bd1b90
Tim <tim@hartauer.org>
2026-03-26 14:17:08 +0100

    Refactor generate function to use localTgzPackage and update output path; add test package

--- AtomicEHRCodegen/codegen-main/codegen-main/examples/local-package-folder/generate.ts
+++ AtomicEHRCodegen/codegen-main/codegen-main/examples/local-package-folder/generate.ts
@@ -8,28 +8,10 @@ async function generateFromLocalPackageFolder() {
     const builder = new APIBuilder();
 
     const report = await builder
-        .localStructureDefinitions({
-            package: { name: "example.folder.structures", version: "0.0.1" },
-            path: Path.join(__dirname, "structure-definitions"),
-            dependencies: [{ name: "hl7.fhir.r4.core", version: "4.0.1" }],
-        })
-        .typescript({})
-        .throwException(true)
-        .typeSchema({
-            treeShake: {
-                "example.folder.structures": {
-                    "http://example.org/fhir/StructureDefinition/ExampleNotebook": {},
-                    "http://example.org/fhir/StructureDefinition/ExampleTypedBundle": {},
-                },
-                "hl7.fhir.r4.core": {
-                    "http://hl7.org/fhir/StructureDefinition/Patient": {},
-                    "http://hl7.org/fhir/StructureDefinition/Organization": {},
-                },
-            },
-        })
-        .introspection({ typeSchemas: "ts/" })
-        .outputTo("./examples/local-package-folder/fhir-types")
-        .generate();
+    .localTgzPackage("./packages/my-custom-ig.tgz")
+    .typescript({})
+    .outputTo("./generated")
+    .generate();
 
     console.log(prettyReport(report));
     if (!report.success) process.exit(1);
